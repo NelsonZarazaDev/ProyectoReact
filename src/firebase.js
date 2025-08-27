65cdb61e8966ec3b5e002697bd2001d8f25c2fba
Nelson Mauricio <nelsonmauricionavarrozaraza@gmail.com>
2025-08-26 19:26:09 -0500

    C=Codigo react

---
+++ src/firebase.js
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signOut } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD8kt8zkBvvCjXPB6ZrZWacUgfK4Ukmblk",
+  authDomain: "projectreactmintic.firebaseapp.com",
+  projectId: "projectreactmintic",
+  storageBucket: "projectreactmintic.firebasestorage.app",
+  messagingSenderId: "622220830201",
+  appId: "1:622220830201:web:56529980a43ffb996f8928"
+};
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+
+// Exportar auth y provider de Google
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+
+// Firestore
+const db = getFirestore(app);
+
+export { auth, googleProvider, db, signOut };
